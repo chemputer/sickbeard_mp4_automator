68d9ef79f15fdf597b834b9601efdfa19d9e9a45
Collision <s.alexlowery@gmail.com>
2018-05-13 00:05:43 -0400

    If cuvid is used to decode a stream into anything other than yuv420, it cannot be done all on hardware, it must be copied to ram.

--- mkvtomp4.py
+++ mkvtomp4.py
@@ -930,7 +930,7 @@ class MkvtoMp4:
             options['preopts'].extend(['-vcodec', 'h264_qsv'])
         elif info.video.codec.lower() in nvenc_cuvid_codecs and \
         self.nvenc_cuvid and vcodec != "copy" and not '422' in info.video.pix_fmt and not '444' in info.video.pix_fmt: #Cuvid only supports 420 chroma at the moment. 
-            if not '10le' in info.video.pix_fmt and not '16le' in info.video.pix_fmt and subtitle_will_be_burned_in == False: #Cannot do full hardware decoding with 10/12 bit video, it must be copied to system memory after decoding.
+            if not '10le' in info.video.pix_fmt and not '16le' in info.video.pix_fmt and subtitle_will_be_burned_in == False and pix_fmt == 'yuv420': #Cannot do full hardware decoding with 10/12 bit video, it must be copied to system memory after decoding.
                 options['preopts'].extend(['-hwaccel', 'cuvid' ])                                                             #Also cannot do full hardware decoding when subtitles are being burned in
                 if info.video.codec.lower() == "hevc" or info.video.codec.lower() == "vp9":
                     if self.nvenc_decoder_hevc_gpu:
